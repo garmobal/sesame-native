193e4e71c73fa8da4f7347f25a36fee1473553c9
garmobal <garmobal@gmail.com>
2021-02-01 10:53:50 +0100

    feat: redux boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import { registrationReducer } from './store/reducers/registration';
+import { doorsReducer } from './store/reducers/doors';
+
+import * as style from './style';
+
+const rootReducer = combineReducers({
+  registration: registrationReducer,
+  doors: doorsReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: style.primaryColor,
     alignItems: 'center',
     justifyContent: 'center',
   },
