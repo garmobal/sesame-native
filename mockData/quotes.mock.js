120adeeba7ec0428a52aed9175250c9b56ebad1b
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-02-02 10:52:15 +0100

    chore: just installed expo-file-system and added a file containing mock quotes

---
+++ mockData/quotes.mock.js
@@ -0,0 +1,8 @@
+export default [
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'First, solve the problem. Then, write the code.',
+  'Experience is the name everyone gives to their mistakes.',
+  'In order to be irreplaceable, one must always be different,',
+  'Java is to JavaScript what car is to Carpet.',
+  'Knowledge is power.',
+];
