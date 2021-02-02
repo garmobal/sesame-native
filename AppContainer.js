9c4c54e72962ff98fa20222553e57ea47e38e71a
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-02-02 11:31:54 +0100

    feat: at app init the file containing the quotes is read/created

--- AppContainer.js
+++ AppContainer.js
@@ -2,17 +2,27 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useDispatch } from 'react-redux';
+import * as FileSystem from 'expo-file-system';
 
 import { fetchDoors } from './store/actions/doorsActions';
+import { fetchQuotes, setQuotes } from './store/actions/quotesActions';
 import Home from './screens/Home';
 import FaceRecognition from './screens/FaceRecognition';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function AppContainer() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const fileUri = FileSystem.documentDirectory + 'quotes.csv';
+    FileSystem.getInfoAsync(fileUri).then((info) => {
+      if (!info.exists) {
+        dispatch(setQuotes(fileUri));
+      } else {
+        dispatch(fetchQuotes(fileUri));
+      }
+    });
     dispatch(fetchDoors());
   }, [dispatch]);
   return (
