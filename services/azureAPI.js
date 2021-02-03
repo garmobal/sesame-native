1b4d41c12acac4c5a99d680110ffc491fb747c71
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-02-03 19:38:46 +0100

    feat: minor change to azureAPI

--- services/azureAPI.js
+++ services/azureAPI.js
@@ -2,7 +2,7 @@
 const key = '6df147ede473450d9c66ac50d3ba8d04'; // Matthieu
 const loc = 'westeurope.api.cognitive.microsoft.com'; // replace with the server nearest to you
 const params =
-  'detect?returnFaceId=true&recognitionModel=recognition_02&detectionModel=detection_02';
+  'detect?returnFaceId=true&recognitionModel=recognition_03&detectionModel=detection_02';
 
 export const detectFace = async (octetStream) => {
   console.log(`https://${loc}/face/v1.0/${params}`);
