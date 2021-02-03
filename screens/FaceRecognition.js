5466a436bded0b0e61a6667331b0d05c54ff1f36
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-02-03 12:38:37 +0100

    refactor: refactor of the FaceRecognition component

--- screens/FaceRecognition.js
+++ screens/FaceRecognition.js
@@ -10,29 +10,48 @@ import { Camera } from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
 import { useSelector } from 'react-redux';
 import base64ToArrayBuffer from 'base64-arraybuffer'; // for converting base64 images to array buffer
-import detectFace from './../services/azureAPI';
+import * as AzureAPI from './../services/azureAPI';
 
 function FaceRecognition() {
+  // CONSTANTS
   const TAKE_SELFIE = 1;
   const CHECKING_FACE = 2;
   const FACE_DETECTED = 3;
   const FACE_NOT_DETECTED = 4;
   const SHOW_QUOTE_TIME = 5000; // [ms]
 
+  // LOCAL STATE
   const [hasPermission, setHasPermission] = useState(null);
   const [faceRecState, setFaceRecState] = useState(TAKE_SELFIE);
   const [detectedFaces, setDetectedFaces] = useState([]);
 
+  // GLOBAL STATE
   const quotes = useSelector((state) => state.quotes);
   const selectedDoor = useSelector((state) => state.selectedDoor);
 
+  // CAMERA REF
   const cam = useRef();
 
+  // HOOKS
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  // HELPER FUNCTIONS
+  /**
+   * Return a random quote from the quotes saved in the .csv file.
+   */
   const _getRandomQuote = () => {
     const index = Math.floor(Math.random() * quotes.length);
     return quotes[index];
   };
 
+  /**
+   * Callback called when a picture is taken.
+   */
   const _takePicture = async () => {
     setFaceRecState(CHECKING_FACE);
 
@@ -42,61 +61,44 @@ function FaceRecognition() {
     };
     try {
       const picture = await cam.current.takePictureAsync(option);
-      const octetStream = base64ToArrayBuffer.decode(picture.base64);
-      const faceDetectRes = await detectFace(octetStream);
-
-      // TODO: send the face id and the selected door to the back end
-      // Wait for the answer
-      // If the user is allowed send the request to open the door
-      if (faceDetectRes.length === 0) {
-        setFaceRecState(FACE_NOT_DETECTED);
-      } else {
-        setFaceRecState(FACE_DETECTED);
-      }
-      setTimeout(() => {
-        setFaceRecState(TAKE_SELFIE);
-      }, SHOW_QUOTE_TIME);
+      _checkPicture(picture);
     } catch (error) {
       console.log('error :>> ', error);
     }
   };
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+  /**
+   * Check if a picture contains a face calling the Azure API.
+   *
+   * @param {object} picture, picture taken with the camera.
+   */
+  const _checkPicture = async (picture) => {
+    const octetStream = base64ToArrayBuffer.decode(picture.base64);
+    const faceDetectRes = await AzureAPI.detectFace(octetStream);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+    // TODO: send the face id and the selected door to the back end
+    // Wait for the answer
+    // If the user is allowed send the request to open the door
+    if (faceDetectRes.length === 0) {
+      setFaceRecState(FACE_NOT_DETECTED);
+    } else {
+      setFaceRecState(FACE_DETECTED);
+    }
+    setTimeout(() => {
+      setFaceRecState(TAKE_SELFIE);
+    }, SHOW_QUOTE_TIME);
+  };
 
-  let testMessage;
-  switch (faceRecState) {
-    case FACE_DETECTED:
-      testMessage = _getRandomQuote();
-      break;
-    case FACE_NOT_DETECTED:
-      testMessage = 'Face not detected';
-      break;
-    case TAKE_SELFIE:
-      testMessage = `Take a selfie to enter ${selectedDoor.name}`;
-      break;
-    case CHECKING_FACE:
-      testMessage = 'We are checking your identity';
-      break;
-    default:
-      testMessage = '';
-  }
-
+  /**
+   * Callback called when a face is detected, it updates the local state.
+   *
+   * @param {array} faces, array containing all the faces detected by the camera.
+   */
   const _handleFacesDetected = ({ faces }) => {
     setDetectedFaces(faces);
   };
 
+  // RENDERING FUNCTIONS
   const renderFace = ({ bounds, faceID, rollAngle, yawAngle }) => {
     return (
       <View
@@ -124,38 +126,76 @@ function FaceRecognition() {
     </View>
   );
 
-  return (
-    <View style={styles.container}>
-      <Camera
-        ref={cam}
-        style={styles.camera}
-        type={Camera.Constants.Type.front}
-        onFacesDetected={_handleFacesDetected}
-        faceDetectorSettings={{
-          mode: FaceDetector.Constants.Mode.fast,
-          detectLandmarks: FaceDetector.Constants.Landmarks.none,
-          runClassifications: FaceDetector.Constants.Classifications.none,
-          minDetectionInterval: 100,
-          tracking: true,
-        }}
-      >
-        <View style={styles.takeButtonContainer}>
-          <TouchableOpacity
-            onPress={_takePicture}
-            style={
-              faceRecState === TAKE_SELFIE
-                ? styles.takeButton
-                : styles.takeButtonDis
-            }
-          />
-        </View>
-      </Camera>
-      {renderFaces()}
+  const renderCameraBtn = () => (
+    <View style={styles.takeButtonContainer}>
+      <TouchableOpacity
+        onPress={_takePicture}
+        style={
+          faceRecState === TAKE_SELFIE
+            ? styles.takeButton
+            : styles.takeButtonDis
+        }
+      />
+    </View>
+  );
+
+  const renderCamera = () => (
+    <Camera
+      ref={cam}
+      style={styles.camera}
+      type={Camera.Constants.Type.front}
+      onFacesDetected={_handleFacesDetected}
+      faceDetectorSettings={{
+        mode: FaceDetector.Constants.Mode.fast,
+        detectLandmarks: FaceDetector.Constants.Landmarks.none,
+        runClassifications: FaceDetector.Constants.Classifications.none,
+        minDetectionInterval: 100,
+        tracking: true,
+      }}
+    >
+      {renderCameraBtn()}
+    </Camera>
+  );
+
+  const renderTestMessage = () => {
+    let testMessage;
+    switch (faceRecState) {
+      case FACE_DETECTED:
+        testMessage = _getRandomQuote();
+        break;
+      case FACE_NOT_DETECTED:
+        testMessage = 'Face not detected';
+        break;
+      case TAKE_SELFIE:
+        testMessage = `Take a selfie to enter ${selectedDoor.name}`;
+        break;
+      case CHECKING_FACE:
+        testMessage = 'We are checking your identity';
+        break;
+      default:
+        testMessage = '';
+    }
+    return (
       <View style={styles.textContainer}>
         <Text style={styles.text}>{testMessage}</Text>
       </View>
-    </View>
-  );
+    );
+  };
+
+  // RENDER
+  if (hasPermission === null) {
+    return <View />;
+  } else if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  } else {
+    return (
+      <View style={styles.container}>
+        {renderCamera()}
+        {renderFaces()}
+        {renderTestMessage()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- services/azureAPI.js
+++ services/azureAPI.js
@@ -2,7 +2,7 @@ const key = 'dab23811cac547258589f18bd4aaf214';
 const loc = 'westeurope.api.cognitive.microsoft.com'; // replace with the server nearest to you
 const params = 'detect?returnFaceId=true&returnFaceAttributes=smile';
 
-export default async (octetStream) => {
+export const detectFace = async (octetStream) => {
   const res = await fetch(`https://${loc}/face/v1.0/${params}`, {
     method: 'POST',
     headers: {
