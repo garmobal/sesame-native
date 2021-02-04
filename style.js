d6db2b1a592fbe6749feb846a7e65d4008ca219c
garmobal <garmobal@gmail.com>
2021-02-04 20:40:43 +0100

    feat: global style sheet

--- style.js
+++ style.js
@@ -1,11 +1,46 @@
+export const globalStyle = {
+  font: 'Roboto',
+};
+
 export const colors = {
   highlight: '#B00E23',
   background: '#F0F0F0',
   fontColor: '#8E8E8E',
+  lightest: '#FFF',
 };
 
-export const redButton = {};
+export const door = {
+  borderWidth: 3,
+  height: '100%',
+  width: 200,
+  marginLeft: 30,
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 15,
+};
 
-export const whiteButton = {};
+export const whiteCard = {
+  borderWidth: 3,
+  height: '100%',
+  width: 200,
+  marginLeft: 30,
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 15,
+};
+
+export const redButton = {
+  fontSize: 30,
+  padding: 10,
+  borderRadius: 5,
+  backgroundColor: colors.highlight,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+export const redButtonText = {
+  fontSize: 30,
+  color: colors.lightest,
+};
 
 export const textInput = {};
