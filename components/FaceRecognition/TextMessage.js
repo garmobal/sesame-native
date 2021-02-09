e6d83416c0c550d002dcb42d330b0ecb5b45c2f4
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-02-09 17:46:21 +0100

    feat: refactor of TextMessage component, added "go back" button

--- components/FaceRecognition/TextMessage.js
+++ components/FaceRecognition/TextMessage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 
 import { useSelector } from 'react-redux';
 import LottieView from 'lottie-react-native';
@@ -12,7 +12,13 @@ import notFound from './../../assets/not_found.png';
 import * as cStyle from './../../style';
 
 const TextMessage = React.memo(
-  ({ userRecState, eUserRecState, userName, selectedDoor }) => {
+  ({
+    userRecState,
+    eUserRecState,
+    userName,
+    selectedDoor,
+    setUserRecState,
+  }) => {
     // GLOBAL STATE
     const quotes = useSelector((state) => state.quotes);
 
@@ -30,62 +36,102 @@ const TextMessage = React.memo(
       setQuote(`"${_getRandomQuote()}"`);
     }, [quotes]);
 
+    const _getImage = () => {
+      let image;
+      switch (userRecState) {
+        case eUserRecState.ALLOWED:
+          image = allowed;
+          break;
+        case eUserRecState.NOT_ALLOWED:
+          image = notAllowed;
+          break;
+        case eUserRecState.NO_USER_FOUND:
+          image = notFound;
+          break;
+        default:
+          image = notFound;
+          break;
+      }
+      return image;
+    };
+
+    const _renderText = () => {
+      let text;
+      switch (userRecState) {
+        case eUserRecState.ALLOWED:
+          text = (
+            <Text style={styles.text}>
+              Welcome,
+              <Text style={[styles.text, styles.textRed]}> {userName}</Text>!
+            </Text>
+          );
+          break;
+        case eUserRecState.NOT_ALLOWED:
+          text = (
+            <Text style={styles.text}>
+              Sorry
+              <Text style={[styles.text, styles.textRed]}> {userName}</Text>,
+              you are not allowed to enter{' '}
+              <Text style={[styles.text, styles.textRed]}>
+                {selectedDoor.doorName}
+              </Text>
+              .
+            </Text>
+          );
+          break;
+        case eUserRecState.NO_USER_FOUND:
+          text = (
+            <Text style={styles.text}>
+              Sorry, we were not able to find you.
+            </Text>
+          );
+          break;
+        default:
+          text = (
+            <Text style={styles.text}>
+              Sorry, we were not able to find you.
+            </Text>
+          );
+          break;
+      }
+      return text;
+    };
+
+    const _renderQuote = () => {
+      if (userRecState === eUserRecState.ALLOWED) {
+        return <Text style={styles.quoteText}>{quote}</Text>;
+      } else {
+        return null;
+      }
+    };
+
     // RENDER
     return (
       <View style={styles.content}>
-        {userRecState === eUserRecState.ALLOWED ? (
+        {userRecState !== eUserRecState.CHECKING_USER ? (
           <>
             <View style={styles.imageContainer}>
-              <Image style={styles.image} source={allowed} />
+              <Image style={styles.image} source={_getImage()} />
             </View>
             <View style={styles.textContainer}>
-              <View style={styles.textCard}>
-                <Text style={styles.text}>
-                  Welcome,
-                  <Text style={[styles.text, styles.textRed]}> Francesco</Text>!
-                </Text>
-              </View>
+              <View style={styles.textCard}>{_renderText()}</View>
             </View>
-
-            <View style={styles.quoteContainer}>
-              <Text style={styles.quoteText}>{quote}</Text>
+            <View style={styles.quoteContainer}>{_renderQuote()}</View>
+            <View style={styles.goBackBtnContainer}>
+              <Pressable
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+                  },
+                  styles.goBackBtn,
+                ]}
+                onPress={() => {
+                  setUserRecState(eUserRecState.TAKE_SELFIE);
+                }}
+              >
+                <Text style={styles.goBackBtnText}>Go back</Text>
+              </Pressable>
             </View>
-          </>
-        ) : null}
-        {userRecState === eUserRecState.NOT_ALLOWED ? (
-          <>
-            <View style={styles.imageContainer}>
-              <Image style={styles.image} source={notAllowed} />
-            </View>
-            <View style={styles.textContainer}>
-              <View style={styles.textCard}>
-                <Text style={styles.text}>
-                  Sorry
-                  <Text style={[styles.text, styles.textRed]}> Francesco</Text>,
-                  you are not allowed to enter{' '}
-                  <Text style={[styles.text, styles.textRed]}>
-                    {selectedDoor.doorName}
-                  </Text>
-                  .
-                </Text>
-              </View>
-            </View>
-            <View style={styles.quoteContainer} />
-          </>
-        ) : null}
-        {userRecState === eUserRecState.NO_USER_FOUND ? (
-          <>
-            <View style={styles.imageContainer}>
-              <Image style={styles.image} source={notFound} />
-            </View>
-            <View style={styles.textContainer}>
-              <View style={styles.textCard}>
-                <Text style={styles.text}>
-                  Sorry, we were not able to find you.
-                </Text>
-              </View>
-            </View>
-            <View style={styles.quoteContainer} />
           </>
         ) : null}
         {userRecState === eUserRecState.CHECKING_USER ? (
@@ -138,6 +184,21 @@ const styles = StyleSheet.create({
     fontFamily: cStyle.fonts.regular_italic,
     fontSize: 20,
     color: cStyle.colors.highlight,
+  },
+  goBackBtnContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  goBackBtn: {
+    ...cStyle.redButton,
+    width: '50%',
+    marginHorizontal: 'auto',
+  },
+  goBackBtnText: {
+    ...cStyle.redButtonText,
+    fontSize: 18,
   },
 });
 
